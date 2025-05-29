8c85bbea97a4b81ea541970a49e431a06d4b1f56
YunMin-Jeong <wda021212@chungbuk.ac.kr>
2025-05-30 00:14:05 +0900

    fix: Flutter 연동을 위한 auth API 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,34 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  //회원가입
+  // 회원가입
   @Post('signup')
   async signup(
-    @Body('userId') userId: string,
+    @Body('name') userId: string,
     @Body('password') password: string,
-    @Body('phoneNumber') phoneNumber: string,
+    @Body('phone') phoneNumber: string,
   ): Promise<string> {
     return this.authService.signup(userId, password, phoneNumber);
   }
 
-  //로그인
+  // 로그인
   @Post('login')
   async login(
-    @Body('userId') userId: string,
+    @Body('id') userId: string,
     @Body('password') password: string,
-  ): Promise<string> {
-    return this.authService.login(userId, password);
+    @Res() res: Response,
+  ) {
+    const result = await this.authService.login(userId, password);
+
+    if (result === '로그인이 완료되었습니다!') {
+      return res.status(HttpStatus.OK).send(result);
+    } else {
+      return res.status(HttpStatus.UNAUTHORIZED).send(result);
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3000; // 포트 번호
+  app.enableCors(); // CORS 전체 허용 (개발용)
   await app.listen(Number(port));
 }
 bootstrap().catch((err) => {
